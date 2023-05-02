b6b44f256dbff7725b5524e94931c171dafaa5cb
Isamh11 <124450003+Isamh11@users.noreply.github.com>
2023-05-02 23:45:39 +0200

    Update for the member menu by Isam
    Our GUI Application for Mental Health Management System with Python and MySQL.
    
    This code is for a mental health management system and includes functions to add, edit, and delete member information in a database. 
    The user interface is created using PyQt5, and the functions interact with a MySQL database using the mysql-connector-python module. 
    The code includes error handling for database queries and displays messages using the QMessageBox module.

--- 01-mindmed.py
+++ 01-mindmed.py
@@ -112,6 +112,147 @@ class MainApp(QMainWindow, ui):
             mydb.close()
 
 
+################### Edit Member ###################
+
+    def show_edit_member_tab(self):
+        self.tabWidget.setCurrentIndex(5)
+        self.choose_member_id_in_comobox()
+
+    def choose_member_id_in_comobox(self):
+        try:
+            mydb = con.connect(host="localhost", user="root", password="Isam2023", db = "MindMed")
+            cursor = mydb.cursor()
+            member_id = self.l5000.text()  # retrieve the text value
+
+            cursor.execute("select * from member where memberID ='"+ member_id +"'")
+
+            result = cursor.fetchall()
+            if result:
+                self.tb501.clear()
+                for mem in result:
+                    self.tb501.addItem(str(mem[0]))
+
+        except con.Error as e:
+            print("Error occurred for the selected employee ID in the combobox" + e)
+        finally:
+            cursor.close()
+            mydb.close()
+           
+
+    def fill_information_of_the_selected_member(self):
+        try:
+            mydb = con.connect(host="localhost", user="root", password="Isam2023", db="MindMed")
+            cursor = mydb.cursor()
+
+            cursor.execute("SELECT * FROM member WHERE memberID = %s", (self.tb501.text(),))
+            result = cursor.fetchone()
+            if result:
+                self.tb222.setText(str(result[1]))
+                self.tb223.setText(str(result[2]))
+                self.cb224.setCurrentText(str(result[3]))
+                self.tb225_1.setText(str(result[4][0:4]))
+                self.tb225_2.setText(str(result[4][5:7]))
+                self.tb225_3.setText(str(result[4][-2:]))
+                self.tb226.setText(str(result[5]))
+                self.tb227.setText(str(result[6]))
+                self.tb228.setText(str(result[7]))
+                self.tb229.setText(str(result[8]))
+                self.cb230.setCurrentText(str(result[9]))
+                self.tb231.setText(str(result[10]))
+            else:
+                QMessageBox.information(self,"Mental Health Management System", "No member found with this ID")
+
+
+        except con.Error as e:
+            print("Error occurred while filling the selected member ID:", e)
+        finally:
+            cursor.close()
+            mydb.close()
+
+
+    def edit_member_information(self):
+        try:
+            mydb = con.connect(host="localhost", user="root", password="Isam2023", db = "MindMed")
+            cursor = mydb.cursor()
+            member_id =self.tb501.text()
+            first_name =self.tb222.text()
+            last_name = self.tb223.text()
+            gender = self.cb224.currentText()
+            date_of_birth = "-".join([self.tb225_1.text(), self.tb225_2.text(), self.tb225_3.text()])
+            mobail_num= self.tb226.text()
+            email = self.tb227.text()
+            address = self.tb228.text()
+            city= self.tb229.text()
+            department = self.cb230.currentText()
+            comment = self.tb231.text()
+            
+            sql_insert_member = "update member set first_name ='"+ first_name +"',last_name ='"+ last_name +"',gender ='"+ gender +"',date_of_birth ='"+ date_of_birth +"',mobail_number ='"+ mobail_num +"',email ='"+ email +"',address ='"+ address +"',city ='"+ city +"',department ='"+ department +"',comment ='"+ comment +"' where memberID = '"+ member_id +"'"
+
+            if(first_name!="" and last_name!="" and gender!="" and date_of_birth!="" and mobail_num!="" and address!="" and city!="" and department!="" and comment!=""):
+                cursor.execute(sql_insert_member)
+                mydb.commit()
+                self.l21.setText("Member information edit successfully")
+                QMessageBox.information(self, "Mental health management system","Member information edit successfully!")
+            else:
+                QMessageBox.information(self,"Mental Health Management System", "Missing Member information, Try again !")
+                self.l220.setText("Missing Member information, Try again !")                  
+            
+            self.tb501.setText("")
+            self.tb222.setText("")
+            self.tb223.setText("")
+            self.cb224.setCurrentText("")
+            self.tb225_1.setText("")
+            self.tb225_2.setText("")
+            self.tb225_3.setText("")
+            self.tb226.setText("")
+            self.tb227.setText("")
+            self.tb228.setText("")
+            self.tb229.setText("")
+            self.cb230.setCurrentText("")
+            self.tb231.setText("")
+            self.tabWidget.setCurrentIndex(1)
+            
+        except con.Error as e:
+            self.l220.setText("Error occured in edit selected member" + e)
+        finally:
+            cursor.close()
+            mydb.close()
+
+
+    def delete_member_information(self):
+        delete_message = QMessageBox.question(self,"Delete", "Are you sure you want to delete this member",QMessageBox.Yes|QMessageBox.No )
+        if delete_message == QMessageBox.Yes:
+            try:
+                mydb = con.connect(host="localhost", user="root", password="Isam2023", db = "MindMed")
+                cursor = mydb.cursor()
+                member_id =self.tb501.text()
+                sql_delete_member = "update member set first_name='Deleted', last_name='Deleted', gender='', date_of_birth='', mobail_number='', email='', address='', city='', department='', comment='Deleted by Admin' where memberID = '"+ member_id +"'"
+                cursor.execute(sql_delete_member)
+                mydb.commit()
+                self.l220.setText("Member information deleted successfully")
+                QMessageBox.information(self, "Mental health management system","Member information deleted successfully!")
+
+                self.tb501.setText("")
+                self.tb222.setText("")
+                self.tb223.setText("")
+                self.cb224.setCurrentText("")
+                self.tb225_1.setText("")
+                self.tb225_2.setText("")
+                self.tb225_3.setText("")
+                self.tb226.setText("")
+                self.tb227.setText("")
+                self.tb228.setText("")
+                self.tb229.setText("")
+                self.cb230.setCurrentText("")
+                self.tb231.setText("")
+                self.tabWidget.setCurrentIndex(1)
+            
+            except con.Error as e:
+                self.l220.setText("Error occured in delete selected member" + e)
+            finally:
+                cursor.close()
+                mydb.close()           
+
 
 def main():
     app = QApplication(sys.argv)
